2ee723936ef3eac9e0cc8691d5cb386105c6be1a
meltstrap <meltstrap@uiiverse.xyz>
2019-10-02 20:17:20 -0300

    Added database structure.

---
+++ DatabaseStructure.sql
@@ -0,0 +1,411 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Oct 02, 2019 at 07:16 PM
+-- Server version: 10.3.16-MariaDB-1:10.3.16+maria~stretch-log
+-- PHP Version: 7.3.9-1~deb10u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uiiverse_db_empty`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_messages`
+--
+
+CREATE TABLE `admin_messages` (
+  `admin_id` int(8) NOT NULL,
+  `admin_type` int(1) NOT NULL,
+  `admin_text` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `admin_to` int(8) NOT NULL,
+  `admin_by` int(8) NOT NULL,
+  `admin_post` int(8) NOT NULL,
+  `is_reply` int(1) NOT NULL,
+  `admin_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `admin_read` int(1) NOT NULL DEFAULT 0
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blacklist`
+--
+
+CREATE TABLE `blacklist` (
+  `source` int(12) NOT NULL,
+  `target` int(12) NOT NULL,
+  `type` int(1) NOT NULL DEFAULT 0,
+  `blacklist_id` bigint(20) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cloudinary_keys`
+--
+
+CREATE TABLE `cloudinary_keys` (
+  `key_id` int(8) NOT NULL,
+  `api_key` bigint(32) NOT NULL,
+  `preset` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `site_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `favorite_titles`
+--
+
+CREATE TABLE `favorite_titles` (
+  `fav_id` int(8) NOT NULL,
+  `user_id` int(8) NOT NULL,
+  `title_id` int(8) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `follows`
+--
+
+CREATE TABLE `follows` (
+  `follow_id` int(8) NOT NULL,
+  `follow_by` int(8) NOT NULL,
+  `follow_to` int(8) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifs`
+--
+
+CREATE TABLE `notifs` (
+  `notif_id` int(8) NOT NULL,
+  `notif_type` int(1) NOT NULL,
+  `notif_by` int(8) DEFAULT NULL,
+  `notif_to` int(8) NOT NULL,
+  `notif_post` int(8) DEFAULT NULL,
+  `merged` int(8) DEFAULT NULL,
+  `notif_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `notif_read` int(1) NOT NULL DEFAULT 0
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(8) NOT NULL,
+  `post_by_id` int(8) NOT NULL,
+  `post_title` int(8) NOT NULL,
+  `deleted` int(1) NOT NULL DEFAULT 0,
+  `feeling_id` int(1) NOT NULL DEFAULT 0,
+  `text` varchar(800) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `post_image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `date_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `post_type` int(8) DEFAULT NULL,
+  `topic` int(8) DEFAULT NULL,
+  `post_drawing` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `spoiler` int(1) DEFAULT 0
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profiles`
+--
+
+CREATE TABLE `profiles` (
+  `user_id` int(8) NOT NULL,
+  `bio` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `name_color` varchar(7) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `country` enum('1','2','3','4','5','6','7') CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `gender` int(1) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `fav_post` int(8) DEFAULT NULL,
+  `organization` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `yeah_notifs` int(1) NOT NULL DEFAULT 1,
+  `last_online` datetime DEFAULT NULL,
+  `skill` enum('1','2','3','4','5','6','7') CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `user_relationship_visibility` int(1) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `replies`
+--
+
+CREATE TABLE `replies` (
+  `reply_id` int(8) NOT NULL,
+  `reply_post` int(8) NOT NULL,
+  `reply_by_id` int(8) NOT NULL,
+  `deleted` int(1) NOT NULL DEFAULT 0,
+  `feeling_id` int(1) NOT NULL DEFAULT 0,
+  `text` varchar(800) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `reply_image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `date_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+CREATE TABLE `reports` (
+  `report_id` bigint(20) NOT NULL,
+  `source` int(12) NOT NULL,
+  `subject` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `type` int(8) NOT NULL DEFAULT 0,
+  `reason` int(8) NOT NULL DEFAULT 0,
+  `message` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `finished` int(8) DEFAULT 0
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `titles`
+--
+
+CREATE TABLE `titles` (
+  `title_id` int(8) NOT NULL,
+  `title_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `title_desc` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `title_icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `title_banner` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `perm` int(1) DEFAULT NULL,
+  `type` int(1) NOT NULL,
+  `user_made` tinyint(1) NOT NULL DEFAULT 0,
+  `title_by` int(8) DEFAULT NULL,
+  `time_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `owner_only` tinyint(1) NOT NULL DEFAULT 0,
+  `title_type` int(8) DEFAULT NULL,
+  `community_title` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(8) NOT NULL,
+  `user_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `user_pass` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `nickname` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `user_face` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `date_created` datetime NOT NULL,
+  `ip` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `user_level` int(1) NOT NULL DEFAULT 0
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `yeahs`
+--
+
+CREATE TABLE `yeahs` (
+  `yeah_id` int(8) NOT NULL,
+  `yeah_post` int(8) NOT NULL,
+  `type` enum('post','reply') CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT 'post',
+  `date_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `yeah_by` int(8) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_messages`
+--
+ALTER TABLE `admin_messages`
+  ADD PRIMARY KEY (`admin_id`),
+  ADD KEY `admin_by` (`admin_by`),
+  ADD KEY `admin_by_2` (`admin_by`),
+  ADD KEY `admin_to` (`admin_to`),
+  ADD KEY `admin_post` (`admin_post`);
+
+--
+-- Indexes for table `blacklist`
+--
+ALTER TABLE `blacklist`
+  ADD PRIMARY KEY (`blacklist_id`),
+  ADD KEY `blibfk1` (`source`),
+  ADD KEY `blibfk2` (`target`);
+
+--
+-- Indexes for table `cloudinary_keys`
+--
+ALTER TABLE `cloudinary_keys`
+  ADD PRIMARY KEY (`key_id`);
+
+--
+-- Indexes for table `favorite_titles`
+--
+ALTER TABLE `favorite_titles`
+  ADD PRIMARY KEY (`fav_id`),
+  ADD UNIQUE KEY `user_id` (`user_id`,`title_id`),
+  ADD KEY `title_id` (`title_id`);
+
+--
+-- Indexes for table `follows`
+--
+ALTER TABLE `follows`
+  ADD PRIMARY KEY (`follow_id`),
+  ADD UNIQUE KEY `follow_by` (`follow_by`,`follow_to`),
+  ADD KEY `follow_to` (`follow_to`);
+
+--
+-- Indexes for table `notifs`
+--
+ALTER TABLE `notifs`
+  ADD PRIMARY KEY (`notif_id`),
+  ADD KEY `notif_by` (`notif_by`),
+  ADD KEY `notif_to` (`notif_to`),
+  ADD KEY `notif_post` (`notif_post`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `post_by_id` (`post_by_id`),
+  ADD KEY `posts_ibfk_2` (`post_title`);
+
+--
+-- Indexes for table `profiles`
+--
+ALTER TABLE `profiles`
+  ADD UNIQUE KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `replies`
+--
+ALTER TABLE `replies`
+  ADD PRIMARY KEY (`reply_id`),
+  ADD KEY `reply_post` (`reply_post`),
+  ADD KEY `reply_by_id` (`reply_by_id`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`report_id`),
+  ADD KEY `repibfk1` (`source`);
+
+--
+-- Indexes for table `titles`
+--
+ALTER TABLE `titles`
+  ADD PRIMARY KEY (`title_id`),
+  ADD KEY `title_by` (`title_by`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_name` (`user_name`);
+
+--
+-- Indexes for table `yeahs`
+--
+ALTER TABLE `yeahs`
+  ADD PRIMARY KEY (`yeah_id`),
+  ADD UNIQUE KEY `yeah_post` (`yeah_post`,`type`,`yeah_by`),
+  ADD KEY `yeah_by` (`yeah_by`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_messages`
+--
+ALTER TABLE `admin_messages`
+  MODIFY `admin_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT for table `cloudinary_keys`
+--
+ALTER TABLE `cloudinary_keys`
+  MODIFY `key_id` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `favorite_titles`
+--
+ALTER TABLE `favorite_titles`
+  MODIFY `fav_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=313;
+
+--
+-- AUTO_INCREMENT for table `follows`
+--
+ALTER TABLE `follows`
+  MODIFY `follow_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=311;
+
+--
+-- AUTO_INCREMENT for table `notifs`
+--
+ALTER TABLE `notifs`
+  MODIFY `notif_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3758;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1000000000;
+
+--
+-- AUTO_INCREMENT for table `replies`
+--
+ALTER TABLE `replies`
+  MODIFY `reply_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99971007;
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `report_id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=454;
+
+--
+-- AUTO_INCREMENT for table `yeahs`
+--
+ALTER TABLE `yeahs`
+  MODIFY `yeah_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13371741;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
