252b4ae1dc993dcb1f04d3d6a127ddb34736ae3e
meltstrap <meltstrap@uiiverse.xyz>
2019-11-30 15:20:56 -0300

    Added 2FA
    	nuevo archivo:  Uiiverse/2fa.php
    	nuevo archivo:  Uiiverse/disable2FA.php
    	modificado:     Uiiverse/enable2FA.php
    	modificado:     Uiiverse/index.php
    	modificado:     Uiiverse/login.php
    	modificado:     Uiiverse/settings.php

---
+++ Uiiverse/disable2FA.php
@@ -0,0 +1,7 @@
+<?php 
+require_once('lib/2fa.php')
+$user_change = $dbc->prepare('UPDATE users SET 2fa_secret = "" AND 2fa_enabled = 0 WHERE users.user_id = ?');
+$user_change->bind_param('ss', $_SESSION['secret'], $_SESSION['user_id']);
+$user_change->execute();
+exit('Two Factor Authentication has been disabled successfully. Redirecting to Uiiverse. <META HTTP-EQUIV="refresh" content="0;URL=/">');
+?>
