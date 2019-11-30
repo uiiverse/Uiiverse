4b8593689cbeea72d0dec68cf8a0c203b0d467ba
meltstrap <meltstrap@uiiverse.xyz>
2019-11-30 18:58:12 -0300

    Fixed disabling 2FA

--- Uiiverse/disable2FA.php
+++ Uiiverse/disable2FA.php
@@ -1,7 +1,9 @@
 <?php 
 require_once('lib/2fa.php');
-$user_change = $dbc->prepare('UPDATE users SET 2fa_secret = "" AND 2fa_enabled = 0 WHERE users.user_id = ?');
-$user_change->bind_param('ss', $_SESSION['secret'], $_SESSION['user_id']);
+require_once('lib/htm.php');
+require_once('lib/htmUsers.php');
+$user_change = $dbc->prepare('UPDATE users SET 2fa_secret = "", 2fa_enabled = 0 WHERE user_id = ?');
+$user_change->bind_param('i', $_SESSION['user_id']);
 $user_change->execute();
-exit('Two Factor Authentication has been disabled successfully. Redirecting to Uiiverse. <META HTTP-EQUIV="refresh" content="0;URL=/">');
+exit('Two Factor Authentication has been disabled successfully.');
 ?>
